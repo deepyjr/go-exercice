bf3393f99edb5dd75ee5d7a0965d63bb085b734d
deepyjr <52158453+deepyjr@users.noreply.github.com>
2023-06-21 15:35:14 +0200

    update the content

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module estiam
 
 go 1.20
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/dlclark/regexp2 v1.2.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/goby-lang/goby v0.1.13 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/lib/pq v1.2.0 // indirect
+	github.com/looplab/fsm v0.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/pkg/profile v1.3.0 // indirect
+	github.com/st0012/metago v0.0.0-20170803060228-9a814882b21a // indirect
+	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
+)
--- main.go
+++ main.go
@@ -1,78 +1,133 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 
-    "estiam/dictionary"
+	"estiam/dictionary"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    d := dictionary.New()
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+	d := dictionary.New()
 
-    for {
-        fmt.Println("\nChoose an action [add/define/remove/list/exit]:")
-        action, _ := reader.ReadString('\n')
-        action = strings.TrimSpace(action)
+	stopServer := make(chan bool) // Canal pour arrêter le serveur Goby
 
-        switch action {
-        case "add":
-            actionAdd(d, reader)
-        case "define":
-            actionDefine(d, reader)
-        case "remove":
-            actionRemove(d, reader)
-        case "list":
-            actionList(d)
-        case "exit":
-            return
-        default:
-            fmt.Println("Unknown action.")
-        }
-    }
+	go runGobyServer(d, stopServer) // Démarrer le serveur Goby en arrière-plan
+
+	runMode("cli", d, reader) // Lancer le mode CLI par défaut
+
+	stopServer <- true // Arrêter le serveur Goby une fois que le mode CLI est terminé
 }
 
 func actionAdd(d *dictionary.Dictionary, reader *bufio.Reader) {
-    fmt.Println("Enter word:")
-    word, _ := reader.ReadString('\n')
-    word = strings.TrimSpace(word)
+	fmt.Println("Enter word:")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
-    fmt.Println("Enter definition:")
-    definition, _ := reader.ReadString('\n')
-    definition = strings.TrimSpace(definition)
+	fmt.Println("Enter definition:")
+	definition, _ := reader.ReadString('\n')
+	definition = strings.TrimSpace(definition)
 
-    d.Add(word, definition)
-    fmt.Println("Added.")
+	d.Add(word, definition)
+	fmt.Println("Added.")
 }
 
 func actionDefine(d *dictionary.Dictionary, reader *bufio.Reader) {
-    fmt.Println("Enter word:")
-    word, _ := reader.ReadString('\n')
-    word = strings.TrimSpace(word)
+	fmt.Println("Enter word:")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
-    entry, err := d.Get(word)
-    if err != nil {
-        fmt.Println("Word not found.")
-    } else {
-        fmt.Println("Definition:", entry.String())
-    }
+	entry, err := d.Get(word)
+	if err != nil {
+		fmt.Println("Word not found.")
+	} else {
+		fmt.Println("Definition:", entry.String())
+	}
 }
 
 func actionRemove(d *dictionary.Dictionary, reader *bufio.Reader) {
-    fmt.Println("Enter word:")
-    word, _ := reader.ReadString('\n')
-    word = strings.TrimSpace(word)
+	fmt.Println("Enter word:")
+	word, _ := reader.ReadString('\n')
+	word = strings.TrimSpace(word)
 
-    d.Remove(word)
-    fmt.Println("Removed.")
+	d.Remove(word)
+	fmt.Println("Removed.")
 }
 
 func actionList(d *dictionary.Dictionary) {
-    words, entries := d.List()
-    for _, word := range words {
-        fmt.Println(word, ":", entries[word].String())
-    }
+	words, entries := d.List()
+	for _, word := range words {
+		fmt.Println(word, ":", entries[word].String())
+	}
+}
+
+func runMode(mode string, d *dictionary.Dictionary, reader *bufio.Reader) {
+	for {
+		fmt.Println("\nChoose an action [add/define/remove/list/exit]:")
+		action, _ := reader.ReadString('\n')
+		action = strings.TrimSpace(action)
+
+		switch action {
+		case "add":
+			actionAdd(d, reader)
+		case "define":
+			actionDefine(d, reader)
+		case "remove":
+			actionRemove(d, reader)
+		case "list":
+			actionList(d)
+		case "exit":
+			if mode == "cli" {
+				return
+			}
+		default:
+			fmt.Println("Unknown action.")
+		}
+	}
+}
+
+func runGobyServer(d *dictionary.Dictionary, stopServer chan bool) {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/define/{word}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		word := vars["word"]
+
+		entry, err := d.Get(word)
+		if err != nil {
+			http.NotFound(w, r)
+		} else {
+			fmt.Fprintf(w, "Definition: %s", entry.String())
+		}
+	}).Methods("GET")
+
+	r.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		word := r.FormValue("word")
+		definition := r.FormValue("definition")
+		d.Add(word, definition)
+		fmt.Fprint(w, "Added.")
+	}).Methods("POST")
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "localhost:8080",
+	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	<-stopServer // Attendre la demande d'arrêt du serveur
+
+	// Arrêter le serveur
+	if err := srv.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
